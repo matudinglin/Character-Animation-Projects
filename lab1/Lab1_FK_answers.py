1786501edff5f8cc69d94a1164064c6c65adf437
matudinglin <45813154+matudinglin@users.noreply.github.com>
2024-01-24 13:39:54 -0800

    Updated task1 part2

--- lab1/Lab1_FK_answers.py
+++ lab1/Lab1_FK_answers.py
@@ -1,22 +1,22 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
+
 def load_motion_data(bvh_file_path):
     """part2 辅助函数，读取bvh文件"""
-    with open(bvh_file_path, 'r') as f:
+    with open(bvh_file_path, "r") as f:
         lines = f.readlines()
         for i in range(len(lines)):
-            if lines[i].startswith('Frame Time'):
+            if lines[i].startswith("Frame Time"):
                 break
         motion_data = []
-        for line in lines[i+1:]:
+        for line in lines[i + 1 :]:
             data = [float(x) for x in line.split()]
             if len(data) == 0:
                 break
-            motion_data.append(np.array(data).reshape(1,-1))
+            motion_data.append(np.array(data).reshape(1, -1))
         motion_data = np.concatenate(motion_data, axis=0)
     return motion_data
-
 
 
 def part1_calculate_T_pose(bvh_file_path):
@@ -30,41 +30,43 @@ def part1_calculate_T_pose(bvh_file_path):
     Tips:
         joint_name顺序应该和bvh一致
     """
+
+    lines = None
+    # Read the file
+    with open(bvh_file_path, "r") as f:
+        lines = f.readlines()
+    # Get the joint name, parent and offset
     joint_name = []
     joint_parent = []
     joint_offset = []
     stack = []
-    # Read the file
-    with open(bvh_file_path, 'r') as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            line = lines[i].lstrip()
-            if line.startswith('ROOT'):
-                joint_name.append(line.split()[1])
-                joint_parent.append(-1)
-                joint_offset.append(np.array([float(x) for x in lines[i+2].split()[1:]]))
-                stack.append(-1)
-            elif line.startswith('JOINT'):
-                joint_name.append(line.split()[1])
-                if len(stack) == 0:
-                    joint_parent.append(-1)
-                else:
-                    joint_parent.append(stack[-1])
-                joint_offset.append(np.array([float(x) for x in lines[i+2].split()[1:]]))
-                stack.append(len(joint_name)-1)
-            elif line.startswith('}'):
-                stack.pop()
-            elif line.startswith('End'):
-                joint_name.append(joint_name[stack[-1]] + '_end')
-                joint_parent.append(stack[-1])
-                joint_offset.append(np.array([float(x) for x in lines[i+2].split()[1:]]))
-                stack.append(len(joint_name)-1)
-            elif line.startswith('MOTION'):
-                break
-    return joint_name, joint_parent, joint_offset
+    for i in range(len(lines)):
+        line = lines[i].lstrip()
+        if line.startswith("ROOT"):
+            joint_name.append(line.split()[1])
+            joint_parent.append(-1)
+            joint_offset.append(np.array([float(x) for x in lines[i + 2].split()[1:]]))
+            stack.append(0)
+        elif line.startswith("JOINT"):
+            joint_name.append(line.split()[1])
+            joint_parent.append(stack[-1])
+            joint_offset.append(np.array([float(x) for x in lines[i + 2].split()[1:]]))
+            stack.append(len(joint_name) - 1)
+        elif line.startswith("}"):
+            stack.pop()
+        elif line.startswith("End"):
+            joint_name.append(joint_name[stack[-1]] + "_end")
+            joint_parent.append(stack[-1])
+            joint_offset.append(np.array([float(x) for x in lines[i + 2].split()[1:]]))
+            stack.append(len(joint_name) - 1)
+        elif line.startswith("MOTION"):
+            break
+    return joint_name, joint_parent, np.array(joint_offset)
 
 
-def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data, frame_id):
+def part2_forward_kinematics(
+    joint_name, joint_parent, joint_offset, motion_data, frame_id
+):
     """请填写以下内容
     输入: part1 获得的关节名字，父节点列表，偏移量列表
         motion_data: np.ndarray，形状为(N,X)的numpy数组，其中N为帧数，X为Channel数
@@ -76,16 +78,41 @@ def part2_forward_kinematics(joint_name, joint_parent, joint_offset, motion_data
         1. joint_orientations的四元数顺序为(x, y, z, w)
         2. from_euler时注意使用大写的XYZ
     """
-    joint_positions = None
-    joint_orientations = None
-    return joint_positions, joint_orientations
+    # Parse the frame data
+    frame_data = motion_data[frame_id].reshape(-1, 3)
+    root_pos = frame_data[0]
+    rot_data = R.from_euler("XYZ", frame_data[1:], degrees=True)
+
+    # Calculate the joint positions and orientations
+    joint_positions = []
+    joint_orientations = []
+    end_count = 0
+    for i, (name, parent, offset) in enumerate(
+        zip(joint_name, joint_parent, joint_offset)
+    ):
+        if name == "RootJoint":
+            joint_positions.append(root_pos)
+            joint_orientations.append(rot_data[0].as_quat())
+        elif name.endswith("_end"):
+            end_count += 1
+            quat = R.from_quat([0, 0, 0, 1])
+            quat_parent = R.from_quat(joint_orientations[parent])
+            joint_orientations.append((quat * quat_parent).as_quat())
+            joint_positions.append(joint_positions[parent] + quat_parent.apply(offset))
+        else:
+            quat = rot_data[i - end_count]
+            quat_parent = R.from_quat(joint_orientations[parent])
+            joint_orientations.append((quat * quat_parent).as_quat())
+            joint_positions.append(joint_positions[parent] + quat_parent.apply(offset))
+
+    return np.array(joint_positions), np.array(joint_orientations)
 
 
 def part3_retarget_func(T_pose_bvh_path, A_pose_bvh_path):
     """
     将 A-pose的bvh重定向到T-pose上
     输入: 两个bvh文件的路径
-    输出: 
+    输出:
         motion_data: np.ndarray，形状为(N,X)的numpy数组，其中N为帧数，X为Channel数。retarget后的运动数据
     Tips:
         两个bvh的joint name顺序可能不一致哦(
